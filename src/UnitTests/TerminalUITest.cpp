b7d265e0e30292a67ccc213e44480490e5269700
danil.korotenko <danil.korotenko@22427a90-aa00-79a6-cd95-da3242c1f8b7>
2010-12-28 14:22:54 +0000

    Added master header for DataModel. Initial revision of terminal UI unit tests.

---
+++ src/UnitTests/TerminalUITest.cpp
@@ -0,0 +1,2 @@
+#include "TerminalUITest.h"
+
---
+++ src/UnitTests/TerminalUITest.h
@@ -0,0 +1,14 @@
+#ifndef TERMINALUITEST_H
+#define TERMINALUITEST_H
+
+#include <QtTest/QtTest>
+
+#include <QPointer>
+
+class TerminalUITest : public QObject
+{
+	Q_OBJECT
+
+};
+
+#endif // TERMINALUITEST_H
